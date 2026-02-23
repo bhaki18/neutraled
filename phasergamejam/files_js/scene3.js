9d820280dcf18e5390732a79a6460b5ab367274a
bhaki18 <angelodelpiano2317@gmail.com>
2026-02-24 00:23:22 +0100

    Implement player animations and movement updates
    Added walking animations and improved player movement logic.

--- phasergamejam/files_js/scene3.js
+++ phasergamejam/files_js/scene3.js
@@ -33,11 +33,26 @@ export class Scene3 extends Phaser.Scene {
     }
 
     preload() {
-        
-        this.load.tilemapTiledJSON('map', 'phasergamejam/assets/scene3/tile_map/map.json');
-        this.load.image('tiles', 'phasergamejam/assets/scene3/tile_map/spritesheet.png');
-        this.load.image('player', 'phasergamejam/assets/scene3/scene3_player.png');
-        this.load.image('npc1', 'phasergamejam/assets/scene3/player.png');
+        this.load.tilemapTiledJSON('map', '/phasergamejam/assets/scene3/tile_map/map.json');
+        this.load.image('tiles', '/phasergamejam/assets/scene3/tile_map/spritesheet.png');
+        this.load.image('player', '/phasergamejam/assets/scene3/scene3_player.png');
+        this.load.image('npc1', '/phasergamejam/assets/scene3/player.png');
+
+        this.load.image('upwalk_frame1', '/phasergamejam/assets/scene3/scene3_upwalking_frame1.png');
+        this.load.image('upwalk_frame2', '/phasergamejam/assets/scene3/scene3_upwalking_frame2.png');
+        this.load.image('upwalk_frame3', '/phasergamejam/assets/scene3/scene3_upwalking_frame3.png');
+
+        this.load.image('leftwalk_frame1', '/phasergamejam/assets/scene3/scene3_leftwalking_frame1.png');
+        this.load.image('leftwalk_frame2', '/phasergamejam/assets/scene3/scene3_leftwalking_frame2.png');
+
+        this.load.image('rightwalk_frame1', '/phasergamejam/assets/scene3/scene3_rightwalking_frame1.png');
+        this.load.image('rightwalk_frame2', '/phasergamejam/assets/scene3/scene3_rightwalking_frame2.png');
+
+
+        this.load.image('downwalk_frame2', '/phasergamejam/assets/scene3/scene3_downwalking_frame2.png');
+        this.load.image('downwalk_frame3', '/phasergamejam/assets/scene3/scene3_downwalking_frame3.png');
+
+
     }
 
     create() {
@@ -71,10 +86,10 @@ export class Scene3 extends Phaser.Scene {
         this.player.setCollideWorldBounds(false);
 
 
-         if (this.registry.get('tutorial_done')){
-        this.player.x = this.registry.get('player_x');
-        this.player.y = this.registry.get('player_y');
-         }
+        if (this.registry.get('tutorial_done')) {
+            this.player.x = this.registry.get('player_x');
+            this.player.y = this.registry.get('player_y');
+        }
 
 
         // camera
@@ -93,16 +108,73 @@ export class Scene3 extends Phaser.Scene {
 
         this.physics.add.collider(this.player, this.npc1);
 
+        // animazioni walking
+
+        this.anims.create({
+            key: 'upwalk', // parola chiave
+            frames: [
+                { key: 'upwalk_frame1' }, // frames dell'animazione
+                { key: 'upwalk_frame2' },
+                { key: 'upwalk_frame1' },
+                { key: 'upwalk_frame3' }
+            ],
+            frameRate: 6, // quanti frame al secondo
+            repeat: -1  // loop infinito
+        });
+
+        this.anims.create({
+            key: 'leftwalk',
+            frames: [
+                { key: 'leftwalk_frame1' },
+                { key: 'leftwalk_frame2' }
+            ],
+            frameRate: 6,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'rightwalk',
+            frames: [
+                { key: 'rightwalk_frame1' },
+                { key: 'rightwalk_frame2' }
+            ],
+            frameRate: 6,
+            repeat: -1
+
+        });
+
+        this.anims.create({
+            key: 'stand',
+            frames: [
+                { key: 'player' },
+            ],
+            frameRate: 6,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'walk',
+            frames: [
+                { key: 'player' },
+                { key: 'downwalk_frame2' },
+                { key: 'downwalk_frame3' }
+            ],
+            frameRate: 6,
+            repeat: -1
+        });
+
 
     }
 
     update() {
 
 
+        let animation_is_playing = false;
         if (this.registry.get('tutorial_done')) {
             this.playerspeed = 120;
             this.npc1.destroy();
 
+            animation_is_playing = false;
 
         }
 
@@ -115,8 +187,15 @@ export class Scene3 extends Phaser.Scene {
 
             this.animation_script();
 
+            animation_is_playing = true;
+
+            this.player.anims.play('stand');
+
         }
 
+        if (!animation_is_playing) {
+            this.walking_script();
+        }
 
 
     }
@@ -135,6 +214,7 @@ export class Scene3 extends Phaser.Scene {
         }
         if (this.keys.up.isDown) {
             this.player.setVelocityY(-this.playerspeed);
+
         }
         if (this.keys.down.isDown) {
             this.player.setVelocityY(this.playerspeed);
@@ -338,7 +418,40 @@ export class Scene3 extends Phaser.Scene {
     }
 
 
+    walking_script() {
+        let anim = null;
+
+        if (this.keys.up.isDown) {
+
+            anim = 'upwalk';
+        }
+        if (this.keys.down.isDown) {
+
+            anim = 'walk';
+        }
+        if (this.keys.left.isDown) {
+
+            anim = 'leftwalk';
+        }
+        if (this.keys.right.isDown) {
+
+            anim = 'rightwalk';
+        }
+
+        if (anim) {
+            if (this.player.anims.currentAnim?.key !== anim) {
+                this.player.anims.play(anim);
+            }
+        } else {
+            this.player.anims.play('stand', true);
+        }
+
+    }
+
 
 }
+
+}
+
 
 
