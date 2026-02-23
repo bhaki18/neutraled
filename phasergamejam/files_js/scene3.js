514d5d7d9ac5b2d42c01432780bb27ef20e017db
bhaki18 <angelodelpiano2317@gmail.com>
2026-02-23 17:51:15 +0100

    Refactor Scene3 class structure and methods

--- phasergamejam/files_js/scene3.js
+++ phasergamejam/files_js/scene3.js
@@ -33,11 +33,11 @@ export class Scene3 extends Phaser.Scene {
     }
 
     preload() {
-        this.load.setBaseURL('phasergamejam/');
-        this.load.tilemapTiledJSON('map', 'assets/scene3/tile_map/map.json');
-        this.load.image('tiles', 'assets/scene3/tile_map/spritesheet.png');
-        this.load.image('player', 'assets/scene3/scene3_player.png');
-        this.load.image('npc1', 'assets/scene3/player.png');
+        
+        this.load.tilemapTiledJSON('map', 'phasergamejam/assets/scene3/tile_map/map.json');
+        this.load.image('tiles', 'phasergamejam/assets/scene3/tile_map/spritesheet.png');
+        this.load.image('player', 'phasergamejam/assets/scene3/scene3_player.png');
+        this.load.image('npc1', 'phasergamejam/assets/scene3/player.png');
     }
 
     create() {
@@ -340,4 +340,5 @@ export class Scene3 extends Phaser.Scene {
 
 
 }
+
 
