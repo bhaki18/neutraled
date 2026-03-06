cf8c8af81b47644e168f6c7aaa4af044b5e2bf5c
bhaki18 <angelodelpiano2317@gmail.com>
2026-03-06 11:32:01 +0100

    Add files via upload

---
+++ phasergamejam/files_js/scene8.js
@@ -0,0 +1,19 @@
+export default class Scene8 extends Phaser.Scene{
+    constructor(){
+        super('Scene8');
+
+    }
+
+    preload(){
+
+    }
+
+    create(){
+
+
+    }
+
+    update(){
+        
+    }
+}
